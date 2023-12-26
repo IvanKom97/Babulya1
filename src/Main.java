be350750bba162031efe21850962ef9eee98566a
Иван <dreamscometrueivan97@gmail.com>
2023-12-26 09:47:37 +0200

    Babulya1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        var Anton = 64;
+        var Sema = 100;
+        var Alesha = 1;
+        var Oleg = 0;
+
+        Anton = Anton + 36;
+        System.out.println("Отымел " + Anton + "телок!");
+
+        var allGirls = Anton + Anton + Oleg + Sema;
+        System.out.println("Всего пацаны отымели " + allGirls + " телочек, кроме Олега!");
+
+
+    }
+}
